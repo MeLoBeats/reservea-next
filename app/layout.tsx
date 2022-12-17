9eb83fb047cd1b9c006560b3709db6b22db49f97
MeLoBeats <lamelobeats@gmail.com>
2022-12-17 16:17:40 +0100

    Default next13 structure

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const layout = ({ children }) => {
+  return (
+    <html lang="fr">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default layout;
